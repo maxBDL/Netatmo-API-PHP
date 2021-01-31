865c33df380276063939fcc295293e94152c89ec
CesarWatrin <cesar.watrin@hotmail.com>
2021-01-31 19:28:31 +0100

    integration javascript

--- Netatmo-API-PHP/script.js
+++ Netatmo-API-PHP/script.js
@@ -12,7 +12,7 @@ L.control.zoom({position:'bottomleft'}).addTo(carte);
 //////////////////////////////////////////////
 /////////////////////////////////////////////
 
-carte.options.maxZoom = 14;
+carte.options.maxZoom = 17;
 carte.options.minZoom = 12;
 
 zone = new L.LayerGroup();
@@ -23,9 +23,12 @@ northEast = bounds.getNorthEast(),
 southWest = bounds.getSouthWest(),
 southEast = bounds.getSouthEast();
 
-// carte.addEventListener('mousedown', () => {
-//    actualiseMap();
-// });
+carte.addEventListener('click', () => {
+   list_villes.innerHTML = '';
+   if (infos.classList[1] === 'active') {
+      infos.classList.value = 'infos';
+   }
+});
 
 // console.log(northWest.lat, northWest.lng);
 // console.log(southWest.lat, southWest.lng);
@@ -65,13 +68,19 @@ var token;
 
 //getpublicdata
 var inputTemp = document.querySelector('#inputTemp');
+var inputHumidity = document.querySelector('#inputHumidity');
 
 function readData(data) {
-   var moy = 0;
+   var moyTemp = 0;
+   var moyHum = 0
+   var humidity;
    for (var i = 0; i < data.length; i++) {
       // getmeasure(data[i]._id, data[i].modules[0], scale, type, data_begin, data_end, limit, optimize, real_time);
       var mod = data[i].measures;
       var temp = data[i].measures[Object.keys(mod)[0]].res;
+      var adresse = data[i].place.location;
+      var altitude = data[i].place.altitude;
+      var pressure = data[i].measures[Object.keys(mod)[1]].res;
       for(var key in temp) {
          var time = new Date(key * 1000);
          var day = time.getDate();
@@ -79,14 +88,26 @@ function readData(data) {
          var year = time.getFullYear();
          var heures = time.getHours();
          var minutes = time.getMinutes();
+         if (minutes >= 0 && minutes <= 9) {
+            minutes = '0' + minutes;
+         }
+         var hum = temp;
          temp = temp[key][0];
-         moy += temp;
+         humidity = hum[key][1];
+         moyTemp += temp;
+         moyHum += humidity;
       }
+      for(var key in pressure) {
+         pressure = pressure[key][0];
+      }
+      hour = heures + 'h' + minutes;
       temp = temp.toString();
-      setMarker(data[i]._id,data[i].place.location[0], data[i].place.location[1], temp);
+      humidity = humidity.toString();
+      setMarker(data[i]._id,data[i].place.location[0], data[i].place.location[1], temp, humidity, pressure, altitude, hour);
       span.className = 'fa fa-location-arrow';
    }
-   inputTemp.value = (moy/data.length).toFixed(2) + '°C';
+   inputTemp.value = (moyTemp/data.length).toFixed(2) + '°C 🌡️';
+   inputHumidity.value = (moyHum/data.length).toFixed(1) + '% 💧';
 }
 
 async function getpublicdata(lat_ne, lon_ne, lat_sw, lon_sw, required_data, filter) {
@@ -188,9 +209,19 @@ var real_time = false;
 
 // getmeasure(device_id, module_id, scale, type, data_begin, data_end, limit, optimize, real_time);
 
-function setMarker(id, lat, lon, temp) {
-   // console.log(lat, lon);
-   var marker = L.marker([lon, lat]).bindTooltip(
+function setMarker(id, lat, lon, temp, hum, press, alt, hour) {
+   var adr = getAdresse([lat, lon]);
+   var markerB = L.icon({
+      iconUrl: 'markerB.png',
+      // shadowUrl: 'leaf-shadow.png',
+
+      iconSize:     [24, 35], // size of the icon
+      // shadowSize:   [50, 64], // size of the shadow
+      // iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+      // shadowAnchor: [4, 62],  // the same for the shadow
+      // popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+   });
+   var marker = L.marker([lon, lat], {icon: markerB}).bindTooltip(
       temp + '°C'
       ,{
          permanent: true,
@@ -200,14 +231,48 @@ function setMarker(id, lat, lon, temp) {
       }
    );
    marker.addEventListener('click', () => {
-      console.log(temp);
+      var adr1 = document.getElementById('adr1');
+      var adr2 = document.getElementById('adr2');
+      var adr3 = document.getElementById('adr3');
+      var tempData = document.getElementById('tempData');
+      var humData = document.getElementById('humData');
+      var pressData = document.getElementById('pressData');
+      var heureMAJ = document.getElementById('heureMAJ');
+      adr1.textContent = adr[0]+', '+adr[1];
+      adr2.textContent = adr[2];
+      adr3.textContent = 'Altitude : ' + alt + 'm';
+      tempData.textContent = temp + '°C';
+      humData.textContent = hum + '%';
+      pressData.textContent = press + ' Hpa';
+      heureMAJ.textContent = hour;
       ////////////////////////////////////
       //RAJOUTER La PROCHAINE LIGNE
       ////////////////////////////////////
       var infos = document.getElementsByClassName('infos')[0];
-      infos.classList.toggle('active');
+      // infos.classList.toggle('active');
+      if (infos.classList[1] === undefined) {
+         infos.classList.value = 'infos active';
+      }
    });
    marker.addTo(zone);
+}
+
+function getAdresse(adr) {
+   var dataAdr;
+   $.ajax({
+      async: false,
+      url: "https://api-adresse.data.gouv.fr/reverse/?lon="+adr[0]+"&lat="+adr[1],
+      success: function(data) {
+         var city = data.features[0].properties.city;
+         var cityCode = data.features[0].properties.citycode;
+         var name = data.features[0].properties.name;
+         dataAdr = [city, cityCode, name];
+      },
+      error: function(data) {
+         console.log('error when search');
+      }
+   });
+   return dataAdr;
 }
 
 var greenIcon = L.icon({
@@ -251,7 +316,7 @@ var ville = document.getElementById('ville');
 var list_villes = document.getElementById('list_villes');
 
 function chargeVilles() {
-   if (ville.value != '') {
+   if (ville.value.replace(/\s+/, '').length) {
       $.ajax({
          url: "https://api-adresse.data.gouv.fr/search/?q="+ville.value+"&limit=10",
          success: function(data) {
@@ -261,6 +326,8 @@ function chargeVilles() {
             console.log('error when search');
          }
       });
+   } else {
+      list_villes.innerHTML = '';
    }
 }
 
@@ -278,18 +345,18 @@ function setAdresse(elem) {
    list_villes.innerHTML = '';
 }
 
-var googleIcon = L.icon({
-   iconUrl: 'locate.png',
+var markerO = L.icon({
+   iconUrl: 'markerO.png',
    // shadowUrl: 'leaf-shadow.png',
 
-   iconSize:     [30, 40], // size of the icon
+   iconSize:     [24, 35], // size of the icon
    // shadowSize:   [50, 64], // size of the shadow
    // iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
    // shadowAnchor: [4, 62],  // the same for the shadow
    // popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
 
-var searchMarker = L.marker([0, 0], {icon: googleIcon}).addTo(carte);
+var searchMarker = L.marker([0, 0], {icon: markerO}).addTo(carte);
 
 function recherche() {
    list_villes.innerHTML = '';
@@ -301,7 +368,7 @@ function recherche() {
             var lat = data.features[0].geometry.coordinates[0];
             var lon = data.features[0].geometry.coordinates[1];
             carte.setView([lon, lat], 14, { animation: true });
-            searchMarker = L.marker([lon, lat], {icon: googleIcon}).addTo(carte);
+            searchMarker = L.marker([lon, lat], {icon: markerO}).addTo(carte);
             actualiseMap();
          },
          error: function(data) {
@@ -315,6 +382,6 @@ function recherche() {
 var close = document.getElementsByClassName('close')[0];
 var infos = document.getElementsByClassName('infos')[0];
 close.addEventListener('click', () => {
-   console.log('ok');
+   // console.log('ok');
    infos.classList.toggle('active');
 });
